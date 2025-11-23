17e800450b530271368d8e5dd56c276eb4527005
cat <242162690+nopecatagnkiaizippolpzopz@users.noreply.github.com>
2025-11-23 12:05:21 +0100

    add the routes to the app

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 import express from "express";
 import doctorsRouter from "../routes/doctors.js";
 import patientsRouter from "../routes/patients.js";
+import appointmentsRouter from "../routes/appointments.js";
+import metricsRouter from "../routes/metrics.js";
 
 const app = express();
 
@@ -8,5 +10,7 @@ app.use(express.json());
 
 app.use("/api/doctors", doctorsRouter);
 app.use("/api/patients", patientsRouter);
+app.use("/api/appointments", appointmentsRouter);
+app.use("/api/metrics", metricsRouter);
 
 export default app;
